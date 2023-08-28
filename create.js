9a625a2352fee2355f10d983435310fb92e719d3
abdulali01 <54444134+abdulali01@users.noreply.github.com>
2023-08-28 15:13:54 -0500

    Create create.js

---
+++ create.js
@@ -0,0 +1,52 @@
+
+//global variables
+var velocityX = 3;
+var velocityy = 3;
+var positionX = 0;
+var positionY = 0;
+var time = 75;
+var positionXMin = 0;
+var positionXMax = 800;
+var positionYMin = 0;
+var positionYMax = 800;
+var reverse_X = true;
+var reverseY = true;
+
+var element = document.getElementById("ball");
+
+function moveBall() {
+
+  if(reverse_X)
+    {
+        positionX = positionX + velocityX;
+        ball.style.left = positionX + 'px';
+    }
+  else
+    {
+        positionX = positionX - velocityX;
+        ball.style.left = positionX + 'px';
+    }
+
+   if(reverseY)
+    {
+        positionY = positionY + velocityy;
+        ball.style.top = positionY + 'px';
+    }
+  else
+    {
+        positionY = positionY - velocityy;
+        ball.style.top = positionY + 'px';
+    }
+  if (positionX > positionXMax || positionX === positionXMin)
+    {
+        reverse_X = !reverse_X;
+    }
+  if (positionY > positionYMax || positionY === positionYMin)
+    {
+        reverseY = !reverseY;
+    }
+    
+
+}
+// This call the moveBall function based on the time designated
+setInterval(moveBall, time);
